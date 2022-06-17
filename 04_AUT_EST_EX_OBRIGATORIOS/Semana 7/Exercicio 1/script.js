8b32475d1c75e6e029bffaf4e83e06bf4acb21d3
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2022-06-17 15:21:41 -0300

    Atualizações ✨

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercicio 1/script.js
@@ -0,0 +1,9 @@
+function adicionar(){
+    var number = document.querySelector("#number")
+    number.value = parseInt(number.value) + 1;
+}
+
+function subtrair(){
+    var number = document.querySelector("#number")
+    number.value = parseInt(number.value) - 1;
+}
